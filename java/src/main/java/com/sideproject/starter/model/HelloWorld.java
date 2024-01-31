a336d9f095389fa643ea292e01ac13eb3372ddc0
Stephen X. Flynn <stephenflynn@me.com>
2024-01-31 12:42:55 -0500

    removed unused imports

--- java/src/main/java/com/sideproject/starter/model/HelloWorld.java
+++ java/src/main/java/com/sideproject/starter/model/HelloWorld.java
@@ -1,9 +1,5 @@
 package com.sideproject.starter.model;
 
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.NotNull;
-import java.math.BigDecimal;
-
 public class HelloWorld {
 
 private String message;
