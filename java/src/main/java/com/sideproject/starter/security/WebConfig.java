1cf5baf6e23daf618ac4731636b05213348daa99
Stephen X. Flynn <stephenflynn@me.com>
2024-01-31 12:43:08 -0500

    added nonnull annotation

--- java/src/main/java/com/sideproject/starter/security/WebConfig.java
+++ java/src/main/java/com/sideproject/starter/security/WebConfig.java
@@ -1,14 +1,15 @@
 package com.sideproject.starter.security;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
